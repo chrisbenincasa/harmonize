c4cd9be11d4267d53058b813ea20d74eb990d639
Christian Benincasa <chrisbenincasa@gmail.com>
2014-08-10 17:17:38 -0400

    move everything to src and change around Gruntfile for eventual build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var request = require('request');
-
 module.exports = function (grunt) {
   // show elapsed time at the end
   require('time-grunt')(grunt);
@@ -12,9 +10,22 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+        build: {
+            src: ['build']
+        }
+    },
+    copy: {
+        build: {
+            cwd: 'src',
+            src: ['**'],
+            dest: 'build',
+            expand: true
+        }
+    },
     develop: {
       server: {
-        file: 'app.js'
+        file: 'src/app.js'
       }
     },
     watch: {
@@ -24,51 +35,40 @@ module.exports = function (grunt) {
       },
       server: {
         files: [
-          'app.js',
-          'routes/*.js'
+          'src/app.js',
+          'src/routes/*.js'
         ],
         tasks: ['develop', 'delayed-livereload']
       },
       js: {
-        files: ['public/js/*.js'],
+        files: ['src/public/js/*.js'],
         options: {
           livereload: reloadPort
         }
       },
       css: {
-        files: ['public/css/*.css'],
+        files: ['src/public/css/*.css'],
         options: {
           livereload: reloadPort
         }
       },
       jade: {
-        files: ['views/*.jade'],
+        files: ['src/views/*.jade'],
         options: {
           livereload: reloadPort
         }
       }
     }
+
   });
 
   grunt.config.requires('watch.server.files');
   files = grunt.config('watch.server.files');
   files = grunt.file.expand(files);
 
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
-          var reloaded = !err && res.statusCode === 200;
-          if (reloaded) {
-            grunt.log.ok('Delayed live reload successful.');
-          } else {
-            grunt.log.error('Unable to make a delayed live reload.');
-          }
-          done(reloaded);
-        });
-    }, 500);
-  });
-
   grunt.registerTask('default', ['develop', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build', 'Build the project for production', ['clean', 'copy']);
 };
